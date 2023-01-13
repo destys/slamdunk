4c0768c7b06ef2cf6d815c4e6de99e30756ee498
destys <73762107+destys@users.noreply.github.com>
2023-01-13 16:23:27 +0300

    Update inputmask.js

--- src/js/files/forms/inputmask.js
+++ src/js/files/forms/inputmask.js
@@ -7,7 +7,8 @@ import { flsModules } from "../modules.js";
 // Подключение модуля
 import "inputmask/dist/inputmask.min.js";
 
-const inputMasks = document.querySelectorAll('input');
-if (inputMasks.length) {
-	flsModules.inputmask = Inputmask().mask(inputMasks);
+const inputMasks = document.querySelectorAll('input[type=tel]');
+if (inputMasks.length > 0) {
+	var im = new Inputmask("+9 (999) 999-99-99");
+	im.mask(inputMasks);
 }
